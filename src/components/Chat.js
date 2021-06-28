bf15b38ed08dc54b9d53b2c1bd416155ee42aa9a
Pradumn Kumar <27pradumn@gmail.com>
2021-06-28 21:16:21 +0530

    ui changes

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import EmojiEmotionsOutlinedIcon from '@material-ui/icons/EmojiEmotionsOutlined';
-import { Mic } from '@material-ui/icons';
+import { Call, Mic, VideoCall } from '@material-ui/icons';
 import Message from "./Message"
+import { Avatar } from '@material-ui/core';
 
 export default function Chat() {
     return (
         <div className="chat">
             <div className="chatheader">
-                hbhs
+                <Avatar />
+                <h3>Group ka Naam</h3>
+                <div className="calls">
+                    <Call/>
+                    <VideoCall/>
+                </div>
             </div>
             <div className="messages">
                 <Message/>
