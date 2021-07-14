be7e4e303218c58531bc2bc6b96f438f6f08059b
Pradumn Kumar <27pradumn@gmail.com>
2021-07-14 22:39:01 +0530

    profile ui 2.0

--- src/components/ProfilePage/Profile.js
+++ src/components/ProfilePage/Profile.js
@@ -8,15 +8,26 @@ import { useStateValue } from "../../StateReducer/StateProvider"
 function Profile() {
     const [{ user },] = useStateValue();
     const [dis, setDis] = useState(false);
-    const [name,setName] = useState(user.displayName);
+    const [name, setName] = useState(user.displayName);
+    const [hov,setHov] = useState(false);
+    const [photo,setPhoto] = useState(user.photoURL);
     useEffect(() => {
         setTimeout(() => {
             setDis(true);
-        }, 3000);
+        }, 2200);
     }, []);
-    const changeName = (e)=>{
+    useEffect(()=>{
+        setPhoto(user.photoURL);
+    },[user.photoURL])
+    const changeName = (e) => {
         e.preventDefault();
         console.log("enter");
+    }
+    const clipped = () => {
+        // For Chrome
+        navigator.clipboard.writeText(123455);
+        // For Older Browser Versions
+        // window.clipboardData.setData("Text", 123455);
     }
 
     return (
@@ -27,22 +38,28 @@ function Profile() {
                     <div className="profile">
                         <p className="line-1 typer">Here's Your Display Profile!!</p>
                         {dis && (
-                            <div className="data">
-                                <div className="dp">
-                                    <h4>Display Picture : </h4>
-                                    <div className="dp-in">
-                                        <Avatar src={user.photoURL} style={{height:"90px" , width:"90px"}}/>
-                                        <h3 className="change">Change your Display Picture</h3>
+                            <div className="elements">
+                                <div className="data">
+                                    <div className="dp">
+                                        <h4>Display Picture : </h4>
+                                        <div className="dp-in">
+                                            <Avatar src={photo} style={{ height: "90px", width: "90px" }} />
+                                            <h3 className="change">Change your Display Picture</h3>
+                                        </div>
+                                    </div>
+                                    <div className="dn">
+                                        <h4>Display Name : </h4>
+                                        <div className="dn-in">
+                                            <form className="dn-form">
+                                                <input className="inpt" defaultValue={name} onChange={(e) => { setName(e.target.value) }}></input>
+                                                <button className="but" type="submit" onClick={changeName}>Save</button>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="dn">
-                                    <h4>Display Name : </h4>
-                                    <div className="dn-in">
-                                        <form className="dn-form">
-                                            <input className="inp" defaultValue={name} onChange={(e)=>{setName(e.target.value)}}></input>
-                                            <button className="but" type="submit" onClick={changeName}>Save</button>
-                                        </form>
-                                    </div>
+                                <div className="invitation">
+                                    <h4>Invitation Code :</h4>
+                                    <h3 className={hov?"hov":""} onMouseEnter={()=>{setHov(true)}} onMouseLeave={()=>{setHov(false)}} onClick={clipped} >{hov?"copy to clipboard":123455}</h3>
                                 </div>
                             </div>)}
                     </div>
