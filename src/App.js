ec9474fd01123b5d14cc076a543a08d1ed609892
Pradumn Kumar <27pradumn@gmail.com>
2021-07-15 23:07:33 +0530

    sign out issue solved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import Routess from "./routess"
 import { useStateValue } from "./StateReducer/StateProvider"
 import { auth } from './Firebase';
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { actionTypes } from './StateReducer/Reducer';
 import Login from "./Auth/Login";
+import Loading from "./Loading/Loading";
 
 function App() {
   const [{ user }, dispatch] = useStateValue();
+  const [isLoading,setIsLoading] = useState(true);
+  const [check,setCheck] = useState(false);
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       dispatch({
         type: actionTypes.SET_USER,
         user: user,
       })
-    })
-  });
+    });
+    user?setCheck(true):setCheck(false);
+    setIsLoading(false);
+  },[user,check]);
   return (
     <>
-      {user ? (
+      {isLoading?<Loading/>:check?(
         <div >
-          <Routess />
+          <Routess check={check}/>
         </div>) : <Login />}
     </>
   );
